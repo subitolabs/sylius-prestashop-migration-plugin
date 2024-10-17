356ca3a81fed28559530158e6b3d140e5d3ef4ac
Thomas Decaux <tdecaux@petalmd.com>
2024-10-17 08:59:57 +0200

    fix(provider): local and currency resource provider

--- src/Provider/Currency/CurrencyResourceProvider.php
+++ src/Provider/Currency/CurrencyResourceProvider.php
@@ -2,12 +2,12 @@
 
 declare(strict_types=1);
 
-namespace Jgrasp\PrestashopMigrationPlugin\Provider\Locale;
+namespace Jgrasp\PrestashopMigrationPlugin\Provider\Currency;
 
 use Jgrasp\PrestashopMigrationPlugin\Provider\ResourceProviderInterface;
 use Jgrasp\PrestashopMigrationPlugin\Provider\WithCodeResourceProviderTrait;
 
-class LocaleResourceProvider implements ResourceProviderInterface
+class CurrencyResourceProvider implements ResourceProviderInterface
 {
     use WithCodeResourceProviderTrait;
 }
---
+++ src/Provider/WithCodeResourceProviderTrait.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Jgrasp\PrestashopMigrationPlugin\Provider;
+
+use ReflectionClass;
+use Jgrasp\PrestashopMigrationPlugin\Attribute\PropertyAttributeAccessor;
+use Jgrasp\PrestashopMigrationPlugin\Model\ModelInterface;
+use Jgrasp\PrestashopMigrationPlugin\Provider\ResourceProviderInterface;
+use Sylius\Component\Core\Formatter\StringInflector;
+use Sylius\Component\Resource\Model\ResourceInterface;
+use Sylius\Component\Resource\Repository\RepositoryInterface;
+
+trait WithCodeResourceProviderTrait {
+    private RepositoryInterface $repository;
+
+    private ResourceProviderInterface $provider;
+
+    private PropertyAttributeAccessor $propertyAttributeAccessor;
+
+    public function __construct(ResourceProviderInterface $provider, RepositoryInterface $repository)
+    {
+        $this->provider                  = $provider;
+        $this->repository                = $repository;
+    }
+
+    /**
+     * Search existing sylius object by code.
+     * Fallback to default getResource method.
+     */
+    public function getResource(ModelInterface $model): ResourceInterface
+    {
+        $modelReflection = new ReflectionClass($model);
+        $modelProperties = $modelReflection->getProperties();
+        $valueCode      = null;
+
+        foreach ($modelProperties as $property) {
+            if ($property->getName() === 'code') {
+                $valueCode = $property->getValue($model);
+            }
+        }
+
+        if (!empty($valueCode)) {
+            $resource = $this->repository->findOneBy(['code' => StringInflector::nameToCode($valueCode)]);
+        }
+
+        if (empty($resource)) {
+            $resource = $this->provider->getResource($model);
+        }
+
+        return $resource;
+    }
+
+}
