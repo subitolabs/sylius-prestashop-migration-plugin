a599a27386f40da706f0dbaf453c7da0caa2c9a3
Thomas Decaux <tdecaux@petalmd.com>
2024-10-17 10:25:00 +0200

    feat(product): add category filter for product repository

--- src/Repository/Product/ProductRepository.php
+++ src/Repository/Product/ProductRepository.php
@@ -10,6 +10,62 @@ use function Doctrine\DBAL\Query\QueryBuilder;
 
 class ProductRepository extends EntityRepository
 {
+    public function findAll(array $criteria = [], int $limit = null, int $offset = null): array
+    {
+        $query = $this
+            ->createQueryBuilder()
+            ->select('*')
+            ->from($this->getTable(), '_table');
+
+        if (!empty($criteria)) {
+            foreach ($criteria as $field => $filter) {
+                if ($field === 'category') {
+                    $query = $query->innerJoin('_table', $this->getPrefix() . 'category_product', 'cat_pro', 'cat_pro.id_product = _table.id_product');
+                    $query = $query->where(
+                        $query->expr()->eq('cat_pro.id_category', $query->expr()->literal($filter))
+                    );
+                } else {
+                    $query = $query->where(
+                        $query->expr()->eq($field, $query->expr()->literal($filter))
+                    );
+                }
+            }
+        }
+
+        if (null !== $limit && null !== $offset) {
+            $query
+                ->setMaxResults($limit)
+                ->setFirstResult($offset);
+        }
+
+        return $this->getConnection()->executeQuery($query->getSQL())->fetchAllAssociative();
+    }
+
+    public function count(array $criteria = []): int
+    {
+        $query = $this->createQueryBuilder();
+        $query
+            ->select(sprintf('COUNT(_table.%s)', $this->getPrimaryKey()))
+            ->from($this->getTable(), '_table');
+
+        if (!empty($criteria)) {
+            foreach ($criteria as $field => $filter) {
+                if ($field === 'category') {
+                    $query = $query->innerJoin('_table', $this->getPrefix() . 'category_product', 'cat_pro', 'cat_pro.id_product = _table.id_product');
+                    $query = $query->where(
+                        $query->expr()->eq('cat_pro.id_category', $query->expr()->literal($filter))
+                    );
+                } else {
+                    $query = $query->where(
+                        $query->expr()->eq($field, $query->expr()->literal($filter))
+                    );
+                }
+            }
+        }
+
+        return (int) $this->getConnection()->executeQuery($query->getSQL())->fetchOne();
+    }
+
     public function findByReference(string $reference): array
     {
         $query = $this->createQueryBuilder();
